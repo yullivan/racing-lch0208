87d83edcca050bb15e80c8fbb96d8fb0abd61a41
hello world <sally2975@naver.com>
2024-12-12 17:55:50 +0900

    //자동차 리스트를 만들고 반복문 작성까지함

---
+++ src/main/java/racing/Car.java
@@ -0,0 +1,20 @@
+package racing;
+
+public class Car {
+
+    private String name;
+    private int 이동거리;
+
+    public Car(String name, int 이동거리) {
+        this.name = name;
+        this.이동거리 = 이동거리;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int get이동거리() {
+        return 이동거리;
+    }
+}
---
+++ src/test/java/racing/Car2.java
@@ -0,0 +1,9 @@
+package racing;
+
+public class Car2 {
+    String Porsche;
+
+    public Car2(String porsche) {
+        Porsche = porsche;
+    }
+}
